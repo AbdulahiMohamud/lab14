3141106738db32f4ff8cfec9e88d4fd2fa32bf30
Micheal Scott Evans <michealsevans20@gmail.com>
2022-03-24 19:00:01 -0500

    added more code to the documents

--- js/cart.js
+++ js/cart.js
@@ -18,23 +18,50 @@ function renderCart() {
   showCart();
 }
 
-// TODO: Remove all of the rows (tr) in the cart table (tbody)
+// DONE: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {}
-
-// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
+let removeTR = document.querySelector('tr');
+element.remove();
+// DONE: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
+  
+  
+  
+  // DONE: Find the table body
+  let tbody = document.querySelector('#cart tbody');
+  
+  // DONE: Iterate over the items in the cart
+  for(let i = 0; i < cart.items.length; i++){
+    let tr = document.createElement('tr');
 
-  // TODO: Find the table body
+    let tdRemove = document.createElement('td');
+    tdRemove.textContent = 'X';
+    tr.appendChild(tdRemove);
 
-  // TODO: Iterate over the items in the cart
+    let tdQuantity = document.createElement('td');
+    tdQuantity.textContent = cart.items[i].quantity;
+    tr.appendChild(tdQuantity);
+
+    let tdItem= document.createElement('td');
+    tdItem.textContent = cart.items[i].product;
+    tr.appendChild(tdItem);
+
+    tbody.appendChild(tr);
+
+  }
+
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
+
+
 }
 
 function removeItemFromCart(event) {
-
+  if(click){
+    cart.removeItem 
+  }
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
@@ -43,3 +70,4 @@ function removeItemFromCart(event) {
 
 // This will initialize the page and draw the cart on screen
 renderCart();
+showCart();
